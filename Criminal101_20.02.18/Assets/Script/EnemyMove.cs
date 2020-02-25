6d1d1980985b905e0317767318d2bb50fdb2972c
luna11woolf <yw3264@nyu.edu>
2020-02-24 22:08:05 -0500

    2.24 Playermovement and EnemyMovement

---
+++ Criminal101_20.02.18/Assets/Script/EnemyMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour
+{
+
+    private NavMeshAgent na;
+    private GameObject[] points;
+    public int cur;
+    
+    void Start()
+    {
+        na = this.GetComponent<NavMeshAgent>();
+        points = GameObject.FindGameObjectsWithTag("movepoint");
+        
+    }
+
+   
+    void Update()
+    {
+        if (na.hasPath == false)
+        {
+            cur = Random.Range(0, points.Length);
+            na.SetDestination(points[cur].transform.position);
+             
+
+        }
+    }
+}
---
+++ Criminal101_20.02.18/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rd;
+
+    public float forwardbackward;
+    public float rightLeft;
+
+    void Start()
+    {
+        rd = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        forwardbackward = Input.GetAxis("Horizontal");
+        rightLeft = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rd.AddForce(transform.forward * -forwardbackward * 0.5f, ForceMode.Impulse);
+
+        rd.AddForce(Vector3.right * rightLeft, ForceMode.Impulse);
+
+        //rd.AddTorque(new Vector3(0, 1, 0) * rightLeft, ForceMode.Impulse);
+    }
+}
