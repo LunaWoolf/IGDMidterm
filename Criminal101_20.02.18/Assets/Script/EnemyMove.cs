6628fabdaf9c5e8dacf6264dfa74334c4c012221
luna11woolf <yw3264@nyu.edu>
2020-02-25 09:57:56 -0500

    2.25 enemy raycast & tail enable disable

--- Criminal101_20.02.18/Assets/Script/EnemyMove.cs
+++ Criminal101_20.02.18/Assets/Script/EnemyMove.cs
@@ -8,13 +8,18 @@ public class EnemyMove : MonoBehaviour
 
     private NavMeshAgent na;
     private GameObject[] points;
-    public int cur;
-    
+    private int cur;
+    public Vector3 ray;
+    private MeshRenderer mr;
+    public Material red;
+
+
     void Start()
     {
         na = this.GetComponent<NavMeshAgent>();
         points = GameObject.FindGameObjectsWithTag("movepoint");
-        
+        ray = this.transform.TransformDirection(Vector3.forward);
+        mr = this.GetComponent<MeshRenderer>();
     }
 
    
@@ -24,8 +29,11 @@ public class EnemyMove : MonoBehaviour
         {
             cur = Random.Range(0, points.Length);
             na.SetDestination(points[cur].transform.position);
-             
+        }
 
+        if (Physics.Raycast(transform.position, ray, 10))
+        {
+            mr.material = red;
         }
     }
 }
---
+++ Criminal101_20.02.18/Assets/Script/playerObeserve.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerObeserve : MonoBehaviour
+{
+    public GameObject enemy;
+    private TrailRenderer trail;
+    
+    void Start()
+    {
+        trail = enemy.GetComponent<TrailRenderer>();
+        
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            trail.enabled = true;
+        }
+    }
+}
