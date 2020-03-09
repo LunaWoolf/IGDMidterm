484978bf76ed1f939f12ac7d153bac5697df6dd7
luna11woolf <yw3264@nyu.edu>
2020-03-09 08:20:01 -0400

    Mar9 GamePlay Update

--- Criminal101_20.02.18/Assets/Script/GameStage/PlayerMovement.cs
+++ Criminal101_20.02.18/Assets/Script/GameStage/PlayerMovement.cs
@@ -6,8 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rd;
 
-    public float forwardbackward;
-    public float rightLeft;
+    //public float forwardbackward;
+    //public float rightLeft;
+    public float moveSpeed;
 
     void Start()
     {
@@ -16,15 +17,33 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        forwardbackward = Input.GetAxis("Horizontal");
-        rightLeft = Input.GetAxis("Vertical");
+        //forwardbackward = Input.GetAxis("Horizontal");
+        //rightLeft = Input.GetAxis("Vertical");
     }
 
     void FixedUpdate()
     {
-        rd.AddForce(transform.forward * -forwardbackward * 0.5f, ForceMode.Impulse);
+        //rd.AddForce(transform.forward * -forwardbackward * 0.5f, ForceMode.Impulse);
 
-        rd.AddForce(Vector3.right * rightLeft, ForceMode.Impulse);
+        //rd.AddForce(Vector3.right * rightLeft, ForceMode.Impulse);
 
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(0, 0, moveSpeed * Time.deltaTime); 
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(0, 0, -moveSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(-moveSpeed * Time.deltaTime, 0 , 0);
+        }
     }
 }
